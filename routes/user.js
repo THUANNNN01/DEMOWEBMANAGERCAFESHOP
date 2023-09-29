161322e652b7419d9d8d57112677da86bbf80e84
THUANNN01 <thuanthuanhuynh123@gmail.com>
2023-09-30 00:14:46 +0700

    project

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const connection = require('../connection');
 const route = express.Router();
 
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const { use } = require('..');
 require('dotenv').config(); 
 
 
@@ -59,6 +61,48 @@ route.post('/login',(req, res)) => {
         }
     }) 
 }
+
+var transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+router.post('/forgotPassword',(req,res)=>{
+    const user = req.body;
+    query = "select email, password from user where email=?";
+    connection.query(query,[user.email],(err,result)=>{
+        if(!err){
+            if(result.length <= 0)
+            {
+                return res.status(200).json({message:"Password sent successfully to your email."});
+            }
+            else{
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'Password by Cafe Management System',
+                    html: '<p><b>Your Login details for Cafe Management System</b><br><b>Email: </b>'+result[0].email+'<br><b>Password: </b>'+result[0].password+'<br><a href="http://localhost:4200/">Click here to login</a></p>'
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error);
+                    }
+                    else{
+                        console.log('Email sent: ' +info.response);
+                    }
+                });
+                return res.status(200).json({message:"Password sent successfully to your email."});
+            }
+
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+})
 
 
 module.exports = router;
